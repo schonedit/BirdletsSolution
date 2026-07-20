5f6b13e1bd1ccd888c09c46c2a9714a2a358443f
schon <schon@LEGION-PC>
2026-07-20 23:38:06 +0200

    Add project files.

---
+++ BirdletsSolution/wwwroot/Scripts/nav.js
@@ -0,0 +1,8 @@
+﻿function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.classList.contains("responsive")) {
+        x.classList.remove("responsive");
+    } else {
+        x.classList.add("responsive");
+    }
+}
---
+++ BirdletsSolution/wwwroot/Scripts/price.js
@@ -0,0 +1,5 @@
+﻿$(document).ready(function () {
+    $(".pricing5 .btn-group .btn").click(function () {
+        $(".pricing5 .monthly, .pricing5 .yearly").toggle();
+    });
+});
